4a53b21609327dc71213b932d475d596971ff8b8
inho.choi <inho.choi@mincoding.co.kr>
2025-07-23 13:23:18 +0900

    [feature] 4

--- baseball_test.cpp
+++ baseball_test.cpp
@@ -4,7 +4,7 @@
 class BaseballFixture : public testing::Test
 {
 public:
-  Baseball game;
+  Baseball game{"123"};
   void assertIllegalArgument(string guessNumber)
   {
     try
@@ -24,6 +24,15 @@ TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase)
   assertIllegalArgument("12");
   assertIllegalArgument("12s");
   assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchNumber)
+{
+  GuessResult result = game.guess("123");
+
+  EXPECT_TRUE(result.solved);
+  EXPECT_EQ(3, result.strikes);
+  EXPECT_EQ(0, result.balls);
 }
 
 int main()
